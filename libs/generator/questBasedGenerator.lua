67f4b5e06d586a760d6680ad9e82f7120195061c
PepeAmpere <machys@gmail.com>
2023-12-29 11:24:33 +0000

    Better zoom for debug, quest generation as a function to allow persistent lua instance in Unreal [https://trello.com/c/tqpZqfbH/]

--- libs/generator/questBasedGenerator.lua
+++ libs/generator/questBasedGenerator.lua
@@ -1,44 +1,49 @@
--- rules of spawning
-local minX = MapExt.MIN_X
-local minY = MapExt.MIN_Y
-local maxX = MapExt.MAX_X
-local maxY = MapExt.MAX_Y
-local tileSize = MapExt.TILE_SIZE
+local function QuestBasedGenerator()
 
--- functions localization
-local GetMapTileKey = MapExt.GetMapTileKey
+  -- rules of spawning
+  local minX = MapExt.MIN_X
+  local minY = MapExt.MIN_Y
+  local maxX = MapExt.MAX_X
+  local maxY = MapExt.MAX_Y
+  local tileSize = MapExt.TILE_SIZE
 
--- global for easier Unreal debugging
--- make map
-levelMap = Map.new(minX, maxX, minY, maxY, tileSize)
+  -- functions localization
+  local GetMapTileKey = MapExt.GetMapTileKey
 
--- generate quests
-local quests = {}
-for _, questID in ipairs(questTypes) do
-  local questDef = questTypesDefs[questID]
-  quests[questID] = {}
-  quests[questID].questDef = questDef
-  if questDef.builders then
-    for _, BuilderFunction in ipairs(questDef.builders) do
-      local buildResult = BuilderFunction(quests[questID], levelMap)
+  -- global for easier Unreal debugging
+  -- make map
+  local levelMap = Map.new(minX, maxX, minY, maxY, tileSize)
+
+  -- generate quests
+  local quests = {}
+  for _, questID in ipairs(questTypes) do
+    local questDef = questTypesDefs[questID]
+    quests[questID] = {}
+    quests[questID].questDef = questDef
+    if questDef.builders then
+      for _, BuilderFunction in ipairs(questDef.builders) do
+        local buildResult = BuilderFunction(quests[questID], levelMap)
+      end
     end
   end
+
+  -- !!! example input below
+
+  -- start tile
+  --[[
+  local startTile = levelMap:GetTile(GetMapTileKey(Vec3(0,0,0)))
+  levelMap:TransformTileToType(startTile, tileTypesDefs["BP_3x3_ritual_room"])
+  ]]--
+
+  -- random kitchen in right top corner
+  --[[
+  local kitchenX = math.random(1,3)*900
+  local kitchenY = math.random(1,3)*900
+  local differentTile = levelMap:GetTile(GetMapTileKey(Vec3(kitchenX,kitchenY,0)))
+  levelMap:TransformTileToType(differentTile, tileTypesDefs["BP_3x3_kitchen"])
+  ]]--
+
+  return levelMap
 end
 
--- !!! example input below
-
--- start tile
---[[
-local startTile = levelMap:GetTile(GetMapTileKey(Vec3(0,0,0)))
-levelMap:TransformTileToType(startTile, tileTypesDefs["BP_3x3_ritual_room"])
-]]--
-
--- random kitchen in right top corner
---[[ 
-local kitchenX = math.random(1,3)*900
-local kitchenY = math.random(1,3)*900
-local differentTile = levelMap:GetTile(GetMapTileKey(Vec3(kitchenX,kitchenY,0)))
-levelMap:TransformTileToType(differentTile, tileTypesDefs["BP_3x3_kitchen"])
-]]--
-
-return levelMap
+return QuestBasedGenerator
--- main.lua
+++ main.lua
@@ -1,10 +1,6 @@
 -- globalize the libs (to integrate both in replit, love 2D and also Unreal)
 -- libs
 
-print(_VERSION)
-if math.type then print(math.type(3)) end
-if love then print(love.getVersion()) end
-
 ArrayExt = require("libs.core.ArrayExt")
 TableExt = require("libs.core.TableExt")
 
@@ -25,14 +21,23 @@ local qResult = require("data.questTypes")
 questTypes = qResult[1]
 questTypesDefs = qResult[2]
 
--- run the levele generation
+-- prepare the level generation
 math.randomseed( os.time() )
-levelMap = require ("libs.generator.questBasedGenerator")
+local MapMakingFunction = require ("libs.generator.questBasedGenerator")
+
+-- run the level generation once
+levelMap = MapMakingFunction()
 
 -- ================================================================ --
 -- ================================================================ --
 -- == UNREAL INTEGRATION WILL NEED ABOVE CODE in the Lua Wrapper == --
 -- saved on this level to avoid saving in Unreal
+
+--[[
+print(_VERSION)
+if math.type then print(math.type(3)) end
+if love then print(love.getVersion()) end
+]]--
 
 -- local tileKey = MapExt.GetMapTileKey(Vec3(0, 0, 0))
 -- TableExt.SaveToFile("levelMapExported.lua", levelMap.nodes)
@@ -49,6 +54,8 @@ images = require("data.imagesInit")
 
 local DRAW_SIZE = 20000
 local camera = Gamera.new(-DRAW_SIZE,-DRAW_SIZE,DRAW_SIZE,DRAW_SIZE)
+camera:setScale(0.1)
+
 local positionCenterX = 0
 local positionCenterY = 0
 camera:setWorld(-5000,-5000,DRAW_SIZE,DRAW_SIZE)
@@ -56,7 +63,7 @@ camera:setWindow(0,0,800,600)
 camera:setPosition(positionCenterX, positionCenterY)
 
 local UI_STATES = {
-pan = false,
+  pan = false,
 }
 
 function love.load()
@@ -64,49 +71,49 @@ function love.load()
 end
 
 function love.update(dt)
-if dt < 1/10 then
-  love.timer.sleep(1/10 - dt)
-end
--- lovebird.update()
+  if dt < 1/10 then
+    love.timer.sleep(1/10 - dt)
+  end
+  -- lovebird.update()
 end
 
 function love.draw()
-love.graphics.setBackgroundColor(192, 192, 192)
---simple rectancle drawing in case we do error in the rest of the code
---love.graphics.setColor(0, 255, 0)
---love.graphics.rectangle("fill", 0, 0, 100, 100)
+  love.graphics.setBackgroundColor(192, 192, 192)
 
-camera:draw(DrawMap.AllTiles)
-camera:draw(DrawMap.AllNodes)
-camera:draw(DrawMap.AllEdges)
-camera:draw(DrawMap.AllPaths)
-camera:draw(DrawMap.AllTileRestrictions)
--- camera:draw(draw.DrawPassLevel)
+  --simple rectancle drawing in case we do error in the rest of the code
+  --love.graphics.setColor(0, 255, 0)
+  --love.graphics.rectangle("fill", 0, 0, 100, 100)
 
-DrawMap.CameraAndCursorPosition(camera)
-DrawMap.DebugControl(camera)
+  camera:draw(DrawMap.AllTiles)
+  camera:draw(DrawMap.AllNodes)
+  camera:draw(DrawMap.AllEdges)
+  camera:draw(DrawMap.AllPaths)
+  camera:draw(DrawMap.AllTileRestrictions)
+
+  DrawMap.CameraAndCursorPosition(camera)
+  DrawMap.DebugControl(camera)
 end
 
 function love.mousepressed(x, y, button) 
-if button == 2 then -- right mouse button
-  UI_STATES.pan = true
-end
+  if button == 2 then -- right mouse button
+    UI_STATES.pan = true
+  end
 end
 function love.mousereleased(x, y, button)
-if button == 2 then -- right mouse button
-  UI_STATES.pan = false
-end
+  if button == 2 then -- right mouse button
+    UI_STATES.pan = false
+  end
 end
 function love.mousemoved(x, y, dx, dy, istouch)
-if UI_STATES.pan then
-  DrawMap.PanMouse(camera, x, y, dx, dy, istouch)
-end
+  if UI_STATES.pan then
+    DrawMap.PanMouse(camera, x, y, dx, dy, istouch)
+  end
 end
 
 function love.wheelmoved(x, y)
-DrawMap.WheelZoom(camera, x, y)
+  DrawMap.WheelZoom(camera, x, y)
 end
 
 function love.keyreleased(key, scancode) 
-DrawMap.ControlKeys(camera, key, scancode)
+  DrawMap.ControlKeys(camera, key, scancode)
 end
