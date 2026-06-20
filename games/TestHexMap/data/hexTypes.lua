ac9aa5a05e664b5141885a92543d3d40482f5dbf
PepeAmpere <machys@gmail.com>
2026-06-20 23:15:54 +0000

    prep for new hexTypes [https://trello.com/c/HaaQJtr7/]

--- games/TestHexMap/data/hexTypes.lua
+++ games/TestHexMap/data/hexTypes.lua
@@ -13,6 +13,8 @@ return nodeTypesDefs
 ]]--
 
 local hexTypesDefs = {
+
+  -- technical types
   Undefined = {
     drawDefs = {
       color = {0.1, 0.1, 0.1, 0.25},
@@ -23,10 +25,131 @@ local hexTypesDefs = {
       color = {0.9, 0.1, 0.1, 0.25},
     }
   },
+
+  -- game types
+  --[[
+  bush_secret_trail = {
+    drawDefs = {
+      color = {0.0, 0.0, 0.0, 0.1},
+    }
+  },
+  bush_local_witch_cardbox_house = {
+    drawDefs = {
+      color = {0.0, 0.0, 0.0, 0.1},
+    }
+  },
+  fence_inner_trail = {
+    drawDefs = {
+      color = {0.0, 0.0, 0.0, 0.1},
+    }
+  },
+  fence_outer_trail = {
+    drawDefs = {
+      color = {0.0, 0.0, 0.0, 0.1},
+    }
+  },
+  grass_trail = {
+    drawDefs = {
+      color = {0.0, 0.0, 0.0, 0.1},
+    }
+  },
+  house_body = {
+    drawDefs = {
+      color = {0.0, 0.0, 0.0, 0.1},
+    }
+  },
+  house_entrance_cellar = {
+    drawDefs = {
+      color = {0.0, 0.0, 0.0, 0.1},
+    }
+  },
+  house_entrance_front = {
+    drawDefs = {
+      color = {0.0, 0.0, 0.0, 0.1},
+    }
+  },
+  house_holes = {
+    drawDefs = {
+      color = {0.0, 0.0, 0.0, 0.1},
+    }
+  },
+  kostka = {
+    drawDefs = {
+      color = {0.0, 0.0, 0.0, 0.1},
+    }
+  },
+  kostka_double_entrance = {
+    drawDefs = {
+      color = {0.0, 0.0, 0.0, 0.1},
+    }
+  },
+  road = {
+    drawDefs = {
+      color = {0.0, 0.0, 0.0, 0.1},
+    }
+  },
+  road_with_bushes = {
+    drawDefs = {
+      color = {0.0, 0.0, 0.0, 0.1},
+    }
+  },
+  sand_trail = {
+    drawDefs = {
+      color = {0.0, 0.0, 0.0, 0.1},
+    }
+  },
+  sandbox = {
+    drawDefs = {
+      color = {0.0, 0.0, 0.0, 0.1},
+    }
+  },
+  street = {
+    drawDefs = {
+      color = {0.0, 0.0, 0.0, 0.1},
+    }
+  },
+  street_with_cars = {
+    drawDefs = {
+      color = {0.0, 0.0, 0.0, 0.1},
+    }
+  },
+  street_with_walkpath = {
+    drawDefs = {
+      color = {0.0, 0.0, 0.0, 0.1},
+    }
+  },
+  table_mountain = {
+    drawDefs = {
+      color = {0.0, 0.0, 0.0, 0.1},
+    }
+  },
+  table_mountain_secret_passage = {
+    drawDefs = {
+      color = {0.0, 0.0, 0.0, 0.1},
+    }
+  },
+  trashbins_shelter = {
+    drawDefs = {
+      color = {0.0, 0.0, 0.0, 0.1},
+    }
+  },
+  trashbins_shelter_roof_access = {
+    drawDefs = {
+      color = {0.0, 0.0, 0.0, 0.1},
+    }
+  },
+  water_castle_secondary_entrance= {
+    drawDefs = {
+      color = {0.0, 0.0, 0.0, 0.1},
+    }
+  },
+  ]]--
+
+
   mapCity = {
     drawDefs = {
       color = {0.5, 0.5, 0.5, 1},
-      icon = love.graphics.newImage(string.gsub(GAME_PATH, "%.", "/") .. "data/img/house.png"),
+      icon = love.graphics.newImage(string.gsub(GAME_PATH, "%.", "/") .. "data/img/house.png"), -- ! engine specific
     }
   },
   mapDesert = {
@@ -48,7 +171,7 @@ local hexTypesDefs = {
   mapMountains = {
     drawDefs = {
       color = {0.7, 0.5, 0.6, 1},
-      icon = love.graphics.newImage(string.gsub(GAME_PATH, "%.", "/") .. "data/img/mountain.png"),
+      icon = love.graphics.newImage(string.gsub(GAME_PATH, "%.", "/") .. "data/img/mountain.png"), -- ! engine specific
     }
   },
   mapSea = {
@@ -56,6 +179,7 @@ local hexTypesDefs = {
       color = {0.1, 0.1, 0.9, 1},
     }
   },
+
 }
 
 local hexTypesArray = {}
